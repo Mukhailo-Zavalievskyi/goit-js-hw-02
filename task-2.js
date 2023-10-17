f5728085965089284a20b391ce57f9356ca502ae
Mukhailo-Zavalievskyi <mishazavalevski@gmail.com>
2023-10-17 15:21:50 +0200

    finish

---
+++ task-2.js
@@ -0,0 +1,12 @@
+function formatMessage(message, maxLength) {
+    let messageLength = message.length;
+    let partMessage = message.slice(0, maxLength);
+    if (messageLength <= maxLength) { return message } else { return `${partMessage}...` }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
